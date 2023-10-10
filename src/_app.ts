4d3a37f4f1d308e90254c0048aa7c131da996f6a
Zonny <elbrac.94@gmail.com>
2023-10-10 14:45:41 -0500

    chore: add input number component

--- src/_app.ts
+++ src/_app.ts
@@ -8,6 +8,7 @@ import Card from 'primevue/card'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
 import Dropdown from 'primevue/dropdown'
+import InputNumber from 'primevue/inputnumber'
 import InputText from 'primevue/inputtext'
 import PrimeVue from 'primevue/config'
 import Skeleton from 'primevue/skeleton'
@@ -28,6 +29,7 @@ export default (app: App) => {
   app.component('Column', Column)
   app.component('DataTable', DataTable)
   app.component('Dropdown', Dropdown)
+  app.component('InputNumber', InputNumber)
   app.component('InputText', InputText)
   app.component('Skeleton', Skeleton)
   app.component('Toast', Toast)
